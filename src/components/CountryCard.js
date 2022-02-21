146522de114dd7da193cbeebd8b3d904f7058549
belabdifatimazohra <fatima.zohra.belabdi@gmail.com>
2022-02-21 20:58:50 +0100

    Add search input

--- src/components/CountryCard.js
+++ src/components/CountryCard.js
@@ -11,7 +11,7 @@ function CountryCard({ srcImg ,country, population, region, capital }) {
         <div>
         <Card
           style={{
-            width: "300px",
+            width: "295px",
             height: "350px",
             margin: "40px",
             position: "relative",
