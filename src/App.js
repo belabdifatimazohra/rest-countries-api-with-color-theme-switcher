f3a6441139c8682323dd5650241b321b97c37e17
belabdifatimazohra <fatima.zohra.belabdi@gmail.com>
2022-02-26 16:55:38 +0100

    remove search component

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './styles/App.css';
 import Navbar from './components/NavBar';
-import Search from "./components/Search";
 import CountryList from "./components/CountryList";
 
 function App() {
@@ -12,8 +11,7 @@ function App() {
       <div className="App">
         <Navbar />
         <Routes>
-          <Route exact path="/" element= {<CountryList />} />         
-          <Route path="/search/:search" element= {<Search />}/>
+          <Route exact path="/" element= {<CountryList />} />   
         </Routes>
       </div>
     </Router>
